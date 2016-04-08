bd296a95bc856ef6c029db69d4326f8fd57dbe88
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2016-04-08 15:36:42 -0400

    Add ShellAI Logic EXCEPT MOVE
    Spiderling::move() currently explodes. uncomment the FIXME line when it's fixed.

--- games/spiders/AI.java
+++ games/spiders/AI.java
@@ -6,8 +6,10 @@ package games.spiders;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Random;
 import joueur.BaseAI;
 
 
@@ -25,6 +27,8 @@ public class AI extends BaseAI {
      */
     public Player player;
 
+    private Random rng;
+
     /**
      * This returns your AI's name to the game server. Just replace the string.
      * @return string of you AI's name
@@ -39,6 +43,7 @@ public class AI extends BaseAI {
      */
     public void start() {
         super.start();
+        rng = new Random();
     }
 
     /**
@@ -53,7 +58,7 @@ public class AI extends BaseAI {
      * This is automatically called when the game ends.
      * You can do any cleanup of you AI here, or do custom logging. After this function returns the application will close.
      * @param  won  true if your player won, false otherwise
-     * @param  name  reason">a string explaining why you won or lost
+     * @param  reason a string explaining why you won or lost
      */
     public void ended(boolean won, String reason) {
         super.ended(won, reason);
@@ -66,7 +71,142 @@ public class AI extends BaseAI {
      * @return Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
      */
     public boolean runTurn() {
-        // Put your game logic here for runTurn
-        return true;
+      // uncomment this line and run it against a
+      // This is ShellAI, it is very simple, and demonstrates how to use all the game objects in Spiders
+      int index = rng.nextInt(player.spiders.size());
+      Spider spider = player.spiders.get(index);
+
+      if (spider.gameObjectName.equals("BroodMother")) {
+        BroodMother broodMother = (BroodMother)spider;
+        // broodmother does two things: make babies, eat babies. metal.
+
+        int choice = rng.nextInt(2);
+        if (choice == 0) {
+          if (broodMother.eggs > 0) { // let's make a baby
+
+            // pick a random spiderling type
+            List<String> spiderlingTypes = Arrays.asList("Cutter", "Weaver", "Spitter");
+            index = rng.nextInt(spiderlingTypes.size());
+            String randomSpiderlingType = spiderlingTypes.get(index);
+
+            System.out.println("Broodmother #" + broodMother.id +
+                               " spawning " + randomSpiderlingType);
+            broodMother.spawn(randomSpiderlingType);
+          }
+        }
+        else {
+          if (broodMother.nest.spiders.size() > 1) { // let's eat a baby
+
+            // pick a random spiderling
+            index = rng.nextInt(broodMother.nest.spiders.size());
+            Spider otherSpider = broodMother.nest.spiders.get(index);
+
+            if (otherSpider != broodMother) { // can't eat yourself
+              System.out.println("Broodmother #" + broodMother.id +
+                                 " consuming " + otherSpider.gameObjectName + " #" + otherSpider.id);
+              broodMother.consume((Spiderling)otherSpider);
+            }
+          }
+        }
+      }
+      else {
+        Spiderling spiderling = (Spiderling)spider;
+        // spiderlings all have two common behaviors (move, attack),
+        // plus behavior one specific to their exact type
+
+        // some actions take time. if a spiderling is still doing a thing
+        // then they can't do another thing. much like undergrads, they
+        // might think they can multitask, but really they can't.
+        if (spiderling.busy.equals("")) {
+
+          int choice = rng.nextInt(3);
+          if (choice == 0) { // move
+            if (spiderling.nest.webs.size() > 0 ) {
+
+              // pick a random web to move to
+              index = rng.nextInt(spiderling.nest.webs.size());
+              Web web = spiderling.nest.webs.get(index);
+
+              System.out.println("Spiderling " + spiderling.gameObjectName +
+                                 " #" + spiderling.id + " moving on Web #" + web.id);
+              // spiderling.move(web); // FIXME currently exploding
+            }
+          }
+          else if (choice == 1) { // attack
+            if (spiderling.nest.spiders.size() > 1) {
+
+              // pick a random spiderling to attack
+              index = rng.nextInt(spiderling.nest.spiders.size());
+              Spider otherSpider = spiderling.nest.spiders.get(index);
+
+              // no friendly fire, please
+              if (!(otherSpider.owner.equals(spiderling.owner))) {
+                System.out.println("Spiderling " + spiderling.gameObjectName +
+                                   " #" + spiderling.id + " attacking " +
+                                   otherSpider.gameObjectName + " #" + otherSpider.id);
+                spiderling.attack((Spiderling)otherSpider);
+              }
+            }
+          }
+          else { // do the unique behavior
+            if (spiderling.gameObjectName.equals("Spitter")) {
+              Spitter spitter = (Spitter)spiderling;
+              Nest enemyNest = player.otherPlayer.broodMother.nest;
+
+              // ensure that the highway from here to there doesn't already exist
+              Web existingWeb = null;
+              for (int i = 0; i < enemyNest.webs.size() ; ++i) {
+                if (enemyNest.webs.get(i).nestA == spitter.nest ||
+                    enemyNest.webs.get(i).nestB == spitter.nest) {
+                  existingWeb = enemyNest.webs.get(i);
+                  break;
+                }
+              }
+
+              if (existingWeb == null) {
+                System.out.println("Spitter #" + spitter.id +
+                                   " spitting to Nest #" + enemyNest.id);
+                spitter.spit(enemyNest);
+              }
+            }
+            else if (spiderling.gameObjectName.equals("Cutter")) {
+              Cutter cutter = (Cutter)spiderling;
+              if (cutter.nest.webs.size() > 0) {
+
+                // pick a random web
+                index = rng.nextInt(cutter.nest.webs.size());
+                Web web = cutter.nest.webs.get(index);
+
+                System.out.println("Cutter #" + cutter.id +
+                                   " cutting Web #" + web.id);
+                cutter.cut(web);
+              }
+            }
+            else if (spiderling.gameObjectName.equals("Weaver")) {
+              Weaver weaver = (Weaver)spiderling;
+              if (weaver.nest.webs.size() > 0) {
+
+                // pick a random web
+                index = rng.nextInt(weaver.nest.webs.size());
+                Web web = weaver.nest.webs.get(index);
+
+                // randomly decide to strengthen or weaken
+                choice = rng.nextInt(2);
+                if (choice == 0) { // strengthen
+                  System.out.println("Weaver #" + weaver.id +
+                                     " strengthening Web #" + web.id);
+                  weaver.strengthen(web);
+                }
+                else {
+                  System.out.println("Weaver #" + weaver.id +
+                                     " weakening Web #" + web.id);
+                  weaver.weaken(web);
+                }
+              }
+            }
+          }
+        }
+      }
+      return true;
     }
 }
