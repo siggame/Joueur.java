b25b266a122641c78759a2fd4d5e743a6391bbab
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-03-31 12:13:16 -0500

    Update docs to bring inline with other Joueur quality docs

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -7,9 +7,24 @@ import os.path
 import subprocess
 import shutil
 import markdown # this is a pip package you'll need
+import re
 from datetime import date
 import sys
 
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
 
 def run(*args, **kwargs):
     error_code = subprocess.call(*args, **kwargs)
@@ -30,9 +45,8 @@ if os.path.isdir(temp_path):
     shutil.rmtree(temp_path)
 
 shutil.copytree("../src/main/java", temp_path)
-temp_readme_html_path = os.path.join(temp_path, 'games', "package.html")
 
-with open(temp_readme_html_path, "w+") as readme_html:
+with open(os.path.join(temp_path, "overview.html"), "w+") as readme_html:
     # TODO: this file is not being picked up, so the readme is not included in the docs
     readme_html.write("<html><head></head><body>" + markdown.markdown(readme_md) + "</body></html>")
 
@@ -50,6 +64,7 @@ get_settings_function = """
     }
 """
 
+game_desc_re = re.compile('(?<= \\* ).*')
 for root, dirnames, filenames in os.walk(os.path.join(temp_path, 'games')):
     for filename in filenames:
         if filename != "AI.java":
@@ -66,8 +81,28 @@ for root, dirnames, filenames in os.walk(os.path.join(temp_path, 'games')):
             ai_file.seek(0)
             ai_file.write(''.join(ai_lines))
 
+    for lower_game_name in dirnames:
+        game_name = lower_game_name[0].upper() + lower_game_name[1:]
+
+        with open(os.path.join('../src/main/java/games/', lower_game_name, 'Game.java'), 'r') as game_file:
+            contents = game_file.read()
+
+        game_desc = game_desc_re.search(contents).group()
+        with open(os.path.normpath(os.path.join(root, lower_game_name, 'package-info.java')), 'w+') as file:
+            file.write("""/**
+ * {game_desc}
+ * <h2>Rules</h2>
+ * <p>
+ * The full game rules for {game_name} can be found on <a target="_blank" href="https://github.com/siggame/Cadre/blob/master/Games/{game_name}/rules.md">GitHub</a>.
+ * <p>
+ * Additional materials, such as the <a target="_blank" href="https://github.com/siggame/Cadre/blob/master/Games/{game_name}/story.md">story</a> and <a target="_blank" href="https://github.com/siggame/Cadre/blob/master/Games/{game_name}/creer.yaml">game template</a> can be found on <a target="_blank" href="https://github.com/siggame/Cadre/blob/master/Games/{game_name}/">GitHub</a> as well.
+ */
+package games.{lower_game_name};
+""".format(game_name=game_name, game_desc=game_desc, lower_game_name=lower_game_name))
+
 try:
     run(('javadoc -d {output_path} '
+        '-overview "{temp_path}/overview.html" '
         '-public '
         '-use -windowtitle "Java Joueur Client Documentation" '
         '-header "<h1>Java Joueur Client Documentation</h1>" '
@@ -80,5 +115,27 @@ try:
     ), shell=True)
 except:
     print("javadoc creation had a problem but should be ok...")
+
+# inject favicon into output
+HEAD_TAG = "<head>"
+shutil.copyfile('favicon.ico', os.path.join(output_path, 'favicon.ico'))
+dot_dirs = len(splitall(output_path))
+for root, dirnames, filenames in os.walk(output_path):
+    root = os.path.normcase(root)
+    for filename in filenames:
+        if not filename.endswith('.html'):
+            continue
+
+        n = (len(splitall(root)) - dot_dirs)
+        with open(os.path.join(root, filename), "r+") as file:
+            contents = file.read()
+            index = contents.find(HEAD_TAG) + len(HEAD_TAG)
+            contents = contents[:index] + """
+<link rel="shortcut icon" href="./{}favicon.ico" />
+""".format("../" * n) + contents[index:]
+            file.seek(0)
+            file.write(contents)
+
+print("Java docs generated...")
 
 shutil.rmtree(temp_path)
