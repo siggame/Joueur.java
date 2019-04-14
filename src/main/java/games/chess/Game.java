01ba4cc606f700bc6a070d436af3258489b887a4
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-04-13 23:14:05 -0500

    Revert "Fixed shells AI set up"
    This reverts commit 0f3d769f5774d74660ec55cde4a029071c65e57a.

---
+++ src/main/java/games/chess/Game.java
@@ -0,0 +1,68 @@
+/**
+ * The traditional 8x8 chess board with pieces.
+ */
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+package games.chess;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import org.json.JSONObject;
+import joueur.Client;
+import joueur.BaseGame;
+import joueur.BaseGameObject;
+
+// <<-- Creer-Merge: imports -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// you can add additional import(s) here
+// <<-- /Creer-Merge: imports -->>
+
+/**
+ * The traditional 8x8 chess board with pieces.
+ */
+public class Game extends BaseGame {
+    /**
+     * Forsyth-Edwards Notation (fen), a notation that describes the game board state.
+     */
+    public String fen;
+
+    /**
+     * The list of [known] moves that have occured in the game, in Standard Algebriac Notation (SAN) format. The first element is the first move, with the last being the most recent.
+     */
+    public List<String> history;
+
+    /**
+     * List of all the players in the game.
+     */
+    public List<Player> players;
+
+    /**
+     * A unique identifier for the game instance that is being played.
+     */
+    public String session;
+
+
+    // <<-- Creer-Merge: fields -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // you can add additional field(s) here. None of them will be tracked or updated by the server.
+    // <<-- /Creer-Merge: fields -->>
+
+
+    /**
+     * Creates a new instance of a Game. Used during game initialization, do not call directly.
+     */
+    protected Game() {
+        super();
+        this.name = "Chess";
+
+        this.history = new ArrayList<String>();
+        this.players = new ArrayList<Player>();
+    }
+
+    // <<-- Creer-Merge: methods -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // you can add additional method(s) here.
+    // <<-- /Creer-Merge: methods -->>
+}
